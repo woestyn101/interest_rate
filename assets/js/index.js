eae32e3d458fed794d5011eccae4db95a28918c6
Marius Du Plooy <woestyn101@gmail.com>
2023-11-24 13:52:19 -0500

    css style

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,13 @@
+function interest(principal, rate, years) {
+  return ((principal * rate) / 100) * years;
+}
+console.log(interest(10000, 3.5, 5)); 
+ 
  document.getElementById("Submit").onclick = function () {
         var theAmount = document.getElementById("amount").value;
         var theRate = document.getElementById("rate").value;
         var theYears = document.getElementById("years").value;
-
-        document.getElementById("one").innerHTML = theAmount;
-        document.getElementById("two").innerHTML = theRate;
-        document.getElementById("three").innerHTML = theYears;
+         var theResult = interest(theAmount, theRate, theYears);
+         document.getElementById("result").innerHTML = theResult;
+        
       };
